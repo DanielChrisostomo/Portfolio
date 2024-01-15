a1f7884709d8fded190eb209a2a451dff66acbb5
DanielChrisostomo <daniluz1515@gmail.com>
2024-01-15 11:15:28 -0300

    CSS responsivo menor que 800px

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -8,39 +8,39 @@ const Home = () => {
       <Title />
       <div className="anime-info">
         <section className="info">
-          <span>Minha jornada</span>
+          <span>Minha jornada </span>
           <br />
-          <span>na programação</span>
+          <span>na programação </span>
           <br />
-          <span>começa em 2023</span>
+          <span>começa em 2023 </span>
           <br />
-          <span>ano no qual inicio</span>
+          <span>ano no qual inicio </span>
           <br />
-          <span>meus estudos em</span>
+          <span>meus estudos em </span>
           <br />
-          <span>Análise e</span>
+          <span>Análise e </span>
           <br />
-          <span>Desenvolvimento</span>
+          <span>Desenvolvimento </span>
           <br />
-          <span>de Sistemas</span>
+          <span>de Sistemas </span>
           <br />
           <span>Atualmente </span>
           <br />
-          <span>meu foco é o</span>
+          <span>meu foco é o </span>
           <br />
-          <span>desenvolvimento web</span>
+          <span>desenvolvimento web </span>
           <br />
-          <span>Front-End</span>
+          <span>Front-End </span>
           <br />
-          <span>e busco desenvolver</span>
+          <span>e busco desenvolver </span>
           <br />
-          <span>sistemas com as</span>
+          <span>sistemas com as </span>
           <br />
-          <span>tecnologias</span>
+          <span>tecnologias </span>
           <br />
-          <span>React JS, JavaScript</span>
+          <span>React JS, JavaScript </span>
           <br />
-          <span>TypeScript e CSS3</span>
+          <span>TypeScript e CSS3 </span>
           <br />
         </section>
       </div>
--- src/Pages/Projetos.tsx
+++ src/Pages/Projetos.tsx
@@ -15,53 +15,57 @@ const Projetos = () => {
   return (
     <>
       <Title />
-      <div className="slide anime-projeto">
-        <h2>Projetos</h2>
-        <Swiper
-          modules={[EffectCards]}
-          effect="cards"
-          slidesPerView={1}
-          pagination={{ clickable: true }}
-        >
-          <SwiperSlide>
-            <div className="slide-card">
-              <a href="https://danielchrisostomo.github.io/GitHub-Busca-ReactJS/">
-                GitHub Busca
-              </a>
-              <img
-                className="slide-item"
-                src={Hubusca}
-                alt="Projeto GitHub Busca"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slide-card">
-              <a href="https://danielchrisostomo.github.io/Rick-and-Morty-API/">
-                Rick and Morty Characters
-              </a>
-              <img
-                className="slide-item"
-                src={RickMorty}
-                alt="Projeto Rick and Morty"
-              />
-            </div>
-          </SwiperSlide>
+      <section className="projeto-container">
+        <div className="slide anime-projeto">
+          <h2>Projetos</h2>
+          <div className="slide-container">
+            <Swiper
+              modules={[EffectCards]}
+              effect="cards"
+              slidesPerView={1}
+              pagination={{ clickable: true }}
+            >
+              <SwiperSlide>
+                <div className="slide-card">
+                  <a href="https://danielchrisostomo.github.io/GitHub-Busca-ReactJS/">
+                    GitHub Busca
+                  </a>
+                  <img
+                    className="slide-item"
+                    src={Hubusca}
+                    alt="Projeto GitHub Busca"
+                  />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="slide-card">
+                  <a href="https://danielchrisostomo.github.io/Rick-and-Morty-API/">
+                    Rick and Morty Characters
+                  </a>
+                  <img
+                    className="slide-item"
+                    src={RickMorty}
+                    alt="Projeto Rick and Morty"
+                  />
+                </div>
+              </SwiperSlide>
 
-          <SwiperSlide>
-            <div className="slide-card">
-              <a href="https://danielchrisostomo.github.io/Login-Page-Engenharia/">
-                Login Page
-              </a>
-              <img
-                className="slide-item"
-                src={LoginPage}
-                alt="Projeto Login Page"
-              />
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
+              <SwiperSlide>
+                <div className="slide-card">
+                  <a href="https://danielchrisostomo.github.io/Login-Page-Engenharia/">
+                    Login Page
+                  </a>
+                  <img
+                    className="slide-item"
+                    src={LoginPage}
+                    alt="Projeto Login Page"
+                  />
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
