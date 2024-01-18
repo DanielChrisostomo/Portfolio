c5fc921dd4596ddf276baabeed815aaff901c484
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-01-18 10:09:24 -0300

    Informações em Home ajustado e adicionada resposividade em telas menores que 450px

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -8,40 +8,25 @@ const Home = () => {
       <Title />
       <div className="anime-info">
         <section className="info">
-          <span>Minha jornada </span>
-          <br />
-          <span>na programação </span>
-          <br />
-          <span>começa em 2023 </span>
-          <br />
-          <span>ano no qual inicio </span>
-          <br />
-          <span>meus estudos em </span>
-          <br />
-          <span>Análise e </span>
-          <br />
-          <span>Desenvolvimento </span>
-          <br />
-          <span>de Sistemas </span>
-          <br />
-          <span>Atualmente </span>
-          <br />
-          <span>meu foco é o </span>
-          <br />
-          <span>desenvolvimento web </span>
-          <br />
-          <span>Front-End </span>
-          <br />
-          <span>e busco desenvolver </span>
-          <br />
-          <span>sistemas com as </span>
-          <br />
-          <span>tecnologias </span>
-          <br />
-          <span>React JS, JavaScript </span>
-          <br />
-          <span>TypeScript e CSS3 </span>
-          <br />
+          <p>
+            <span>Minha jornada </span>
+            <span>na programação </span>
+            <span>começa em 2023 </span>
+            <span>ano no qual inicio </span>
+            <span>meus estudos em </span>
+            <span>Análise e </span>
+            <span>Desenvolvimento </span>
+            <span>de Sistemas </span>
+            <span>Atualmente </span>
+            <span>meu foco é o </span>
+            <span>desenvolvimento web </span>
+            <span>Front-End </span>
+            <span>e busco desenvolver </span>
+            <span>sistemas com as </span>
+            <span>tecnologias </span>
+            <span>React JS, JavaScript </span>
+            <span>TypeScript e CSS3 </span>
+          </p>
         </section>
       </div>
     </>
